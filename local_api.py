0600cc3e99de7e03da1adc4686e9fc09f8917831
wgu-areinhart <agrout4@wgu.edu>
2025-05-26 23:01:06 +0000

    Complete the local_api.py code

--- local_api.py
+++ local_api.py
@@ -3,14 +3,13 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+url = "http://127.0.0.1:8000"
+r = requests.get(url)
 
 # TODO: print the status code
-# print()
+print(f"Status Code: {r.status_code}")
 # TODO: print the welcome message
-# print()
-
-
+print(f"Result: {r.json()}")
 
 data = {
     "age": 37,
@@ -30,9 +29,9 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post(f"{url}/data/", json=data)
 
 # TODO: print the status code
-# print()
+print(f"Status Code: {r.status_code}")
 # TODO: print the result
-# print()
+print(f"Result: {r.json()}")
