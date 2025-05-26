c417403936dcc51cdd469bed3fb2629bb75725eb
wgu-areinhart <agrout4@wgu.edu>
2025-05-26 20:34:48 +0000

    Completed code through the inference function

--- train_model.py
+++ train_model.py
@@ -16,11 +16,16 @@ from ml.model import (
 project_path = "Your path here"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = None # your code here
+data = pd.read_csv("data/census.csv")
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = None, None# Your code here
+train, test = train_test_split(
+    data,
+    test_size=0.20,
+    random_state=42,
+    stratify=data["salary"]
+)
 
 # DO NOT MODIFY
 cat_features = [
@@ -36,10 +41,10 @@ cat_features = [
 
 # TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
-    # your code here
-    # use the train dataset 
-    # use training=True
-    # do not need to pass encoder and lb as input
+    train,
+    categorical_features=cat_features,
+    label="salary",
+    training=True
     )
 
 X_test, y_test, _, _ = process_data(
@@ -52,7 +57,7 @@ X_test, y_test, _, _ = process_data(
 )
 
 # TODO: use the train_model function to train the model on the training dataset
-model = None # your code here
+model = train_model(X_train, y_train)
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
@@ -66,7 +71,15 @@ model = load_model(
 ) 
 
 # TODO: use the inference function to run the model inferences on the test dataset.
-preds = None # your code here
+X_test, y_test, _, _ = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
+preds = inference(model, X_test)
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
